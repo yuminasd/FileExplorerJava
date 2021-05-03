0e32c01ea5ed0cf5dc5dbf93eca197bf7782320a
yuminasd <43660270+yuminasd@users.noreply.github.com>
2021-05-03 17:45:00 -0600

    FileExplorer Starter
    starter code for the java application

---
+++ CreateChildNodes.java
@@ -0,0 +1,40 @@
+import java.io.File;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+//Starts from the given file root and then generates the tree
+public class CreateChildNodes implements Runnable {
+
+    private DefaultMutableTreeNode root;
+
+    private File fileRoot;
+
+    public CreateChildNodes(File fileRoot, 
+            DefaultMutableTreeNode root) {
+        this.fileRoot = fileRoot;
+        this.root = root;
+    }
+
+    @Override
+    public void run() {
+        //recursion calls itself until it can't keep going deeper (files ==null)
+        createChildren(fileRoot, root);
+    }
+
+    private void createChildren(File fileRoot, 
+            DefaultMutableTreeNode node) {
+                //lists the files at the current node
+        File[] files = fileRoot.listFiles();
+        if (files == null) return;
+                //for each file that gets listed, add it to the tree and then if it is a directory, call this function with the node as the root
+        for (File file : files) {
+            DefaultMutableTreeNode childNode = 
+                    new DefaultMutableTreeNode(new FileNode(file));
+            node.add(childNode);
+            if (file.isDirectory()) {
+                createChildren(file, childNode);
+            }
+        }
+    }
+
+}
---
+++ FileBrowser.java
@@ -0,0 +1,105 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.SwingUtilities;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+
+import java.awt.Desktop;
+
+public class FileBrowser implements Runnable {
+
+    private DefaultMutableTreeNode root;
+
+    private DefaultTreeModel treeModel;
+
+    private JTree tree;
+
+    private Desktop desktop;
+
+    private String value;
+
+    @Override
+    public void run() {
+        // Creates a Jframe (UI stuff)
+        JFrame frame = new JFrame("File Browser");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Instantiate java Desktop
+        if (!Desktop.isDesktopSupported()) {
+            System.out.println("Desktop not supported");
+            // not sure if this is correct but delete the JFrame
+            frame.setVisible(false); // you can't see me!
+            frame.dispose(); // Destroy the JFrame object
+            return;
+        }
+        desktop = Desktop.getDesktop();
+
+        // get first directory(a node), then creates a tree from the node.
+        File fileRoot = new File("C:/");
+        root = new DefaultMutableTreeNode(new FileNode(fileRoot));
+        treeModel = new DefaultTreeModel(root);
+
+        // goes from a generic tree node into a JTree which is usable with the Jframe
+        // (UI)
+        tree = new JTree(treeModel);
+        tree.setShowsRootHandles(true);
+
+        // add listener for opening files
+        tree.addTreeSelectionListener(new TreeSelectionListener() { // https://docs.oracle.com/javase/tutorial/uiswing/events/treeselectionlistener.html
+            public void valueChanged(TreeSelectionEvent e) {
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+                // nothing is selected
+                if (node == null)
+                    return;
+
+                // if node is a leaf and double click open
+                if (node.isLeaf()) {
+
+                    // get the path
+                    TreePath treepath = e.getPath();
+                    // System.out.println("Java: " + treepath.getLastPathComponent());
+                    Object elements[] = treepath.getPath();
+                    for (int i = 0, n = elements.length; i < n; i++) {
+                        // System.out.print("->" + elements[i]);
+                        value += elements[i] + "\\";
+                    }
+                   // System.out.println(value.substring(4));
+                    // do something with the above object
+
+                    File test = new File(value.substring(4));
+                    try {
+                        if (test.exists())
+                            desktop.open(test);
+                    } catch (IOException sex) {
+                        sex.printStackTrace();
+                    }
+
+                }
+
+            }
+        });
+        JScrollPane scrollPane = new JScrollPane(tree);
+
+        // Add UI elements
+        frame.add(scrollPane);
+        frame.setLocationByPlatform(true);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+
+        // Instantiates a CreateChildNode
+        CreateChildNodes ccn = new CreateChildNodes(fileRoot, root);
+        new Thread(ccn).start();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new FileBrowser());
+    }
+
+}
